a7702c4409c1d1f0d63087e7600f71a39266dbb6
Aashir <aashirsaroya@gmail.com>
2020-04-15 20:48:40 +0530

    Added output results based on calculating total score

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -16,18 +16,30 @@ class _MyAppState extends State
 {
   var questions = [
     {'questionText':'What\'s your favourite colour?',
-      'answer': ['Black','Red','White','Green']
+      'answer': [{'text':'Black','score':10},{'text':'Red','score':4},{'text':'White','score':'2'},
+        {'text':'Green','score':6}]
     },
     {'questionText':'Whats\'s your favourite animal?',
-      'answer':['Dog','Cat','Rabbit','Lion']
+      'answer':[{'text':'Dog','score':3},{'text':'Cat','score':5},{'text':'Rabbit','score':7},
+        {'text':'Lion','score':4}]
     },
     {'questionText': 'What\'s your favourite book?',
-      'answer': ['Harry Potter','Narnia','The Godfather','The Last Don']
+      'answer': [{'text':'Harry Potter','score':5},{'text':'Narnia','score':6},{'text':'The Godfather','score':8},
+        {'text':'The Last Don','score':10}]
     }
   ];
    var questionindex = 0;
-  void answerQuestion()
+   var totalscore = 0;
+   void restartQuiz()
+   {
+     setState(() {
+       questionindex = 0;
+       totalscore = 0;
+     });
+   }
+  void answerQuestion(int score)
   {
+    totalscore = totalscore + score;
     setState(() {
       questionindex = questionindex + 1;
     });
@@ -40,7 +52,7 @@ class _MyAppState extends State
       appBar: AppBar(title:Text('Quiz App') ,backgroundColor: Colors.green,),
       body: questionindex < questions.length ?
       Quiz(answerQuestion: answerQuestion,questionIndex: questionindex,questions: questions,)
-      : Result(),
+      : Result(totalscore),
     )
     );
   }
---
+++ quiz/lib/result.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+class Result extends StatelessWidget {
+  final int resultscore;
+  Result(this.resultscore);
+  String get resultPhrase
+  {
+    var resultText = 'You did it!!';
+    if(resultscore <= 8)
+      resultText = 'You are awesome and innocent:):):)';
+    else if(resultscore <= 12)
+      resultText = 'Preety Likeable :)';
+    else if(resultscore <= 16)
+      resultText = 'You are quite..... strange?';
+    else
+      resultText = 'You are so bad :(:(';
+    return resultText;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:
+      Column(
+        children: <Widget>[
+          Text(
+              resultPhrase,
+            style:TextStyle(fontSize: 36,fontWeight: FontWeight.bold) ,
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(child: Text('Restart Quiz!!'),onPressed: ,)
+        ],
+      ),
+    );
+  }
+}
